d74495a3199b87defd80bb7027850fb18c56d812
sergey <frants_s@bk.ru>
2022-11-07 19:23:36 +0300

    кубы чисел

---
+++ Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу кубов чисел от 1 до N.
+//5 ->
+//1 | 1
+//2 | 8
+//3 | 27
+//4 | 64
+//5 | 125
+
+void Cube(int n)
+{
+    if (n > 0)
+    {
+         int index = 1;
+         while (index <= n)
+        {
+            Console.WriteLine($"{index} {index*index*index}");
+            index++;
+        }
+    }
+    else Console.WriteLine("неверное число");
+}
+
+Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Cube(n);
